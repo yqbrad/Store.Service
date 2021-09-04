fb4d3a4cd883e6df770640f4889d0e482b01a29d
M Yaqubi Rad <yqb.rad@gmail.com>
2021-09-04 20:31:07 +0430

    Product option entity type builder completed.

--- 02 Infrastractures/Infrastructures.DataAccess/ProductOptionAgg/ProductOptionConfig.cs
+++ 02 Infrastractures/Infrastructures.DataAccess/ProductOptionAgg/ProductOptionConfig.cs
@@ -8,7 +8,24 @@ namespace Store.Infrastructure.DataAccess.ProductOptionAgg
     {
         public void Configure(EntityTypeBuilder<ProductOption> builder)
         {
+            builder.HasKey(s => s.Id);
 
+            builder.Property(s => s.Id)
+                .ValueGeneratedNever();
+
+            builder.Property(s => s.Name)
+                .HasConversion(s => s.Value, s => new(s))
+                .HasMaxLength(200)
+                .IsRequired()
+                .IsUnicode();
+
+            builder.Property(s => s.Description)
+                .HasConversion(s => s.Value, s => new(s))
+                .IsRequired()
+                .IsUnicode();
+
+            builder.HasIndex(s => s.Name)
+                .IsUnique();
         }
     }
 }
