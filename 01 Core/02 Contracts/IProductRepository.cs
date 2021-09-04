b5e937241d19e574cfb741232e1bc156d629f4ae
M Yaqubi Rad <yqb.rad@gmail.com>
2021-09-04 19:42:13 +0430

    Product repository interface created.

---
+++ 01 Core/02 Contracts/IProductRepository.cs
@@ -0,0 +1,20 @@
+﻿using Store.DomainModels.ProductAgg.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Store.Contracts
+{
+    public interface IProductRepository
+    {
+        Task CreateAsync(Product product);
+        Task UpdateAsync(Product product);
+        Task DeleteAsync(Product product);
+
+        Task<Product> GetAsync(Guid id);
+        Task<Product> GetIncludeOptionsAsync(Guid id);
+        Task<bool> IsExist(string name);
+        Task<IEnumerable<Product>> GetAllAsync();
+        Task<IEnumerable<Product>> SearchAsync(string name);
+    }
+}
