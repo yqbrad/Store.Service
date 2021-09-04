b1f07273d967cd5adc4553d628ffcd6d8894e6ba
M Yaqubi Rad <yqb.rad@gmail.com>
2021-09-04 19:42:22 +0430

    ProductOption repository interface created.

---
+++ 01 Core/02 Contracts/IProductOptionRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Store.DomainModels.ProductOptionAgg.Entities;
+
+namespace Store.Contracts
+{
+    public interface IProductOptionRepository
+    {
+        Task AddAsync(ProductOption product);
+        Task UpdateAsync(ProductOption product);
+        Task DeleteAsync(ProductOption product);
+
+        Task<ProductOption> GetAsync(Guid id);
+        Task<ProductOption> GetIncludeProductAsync(Guid id);
+        Task<IEnumerable<ProductOption>> GetProductOptionsAsync(Guid productId);
+    }
+}
