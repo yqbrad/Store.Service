a8cda8372ef7b91a89aa1a3633a4613af5cf7f1b
M Yaqubi Rad <yqb.rad@gmail.com>
2021-09-04 21:26:55 +0430

    GetAllProductOptions handler created.

---
+++ 01 Core/04 ApplicationServices/ProductOptionAgg/Request/GetAllProductOptionsHandlerAsync.cs
@@ -0,0 +1,27 @@
+﻿using Framework.Domain.EventBus;
+using Store.ApplicationServices._Base;
+using Store.Contracts._Base;
+using Store.DomainModels.ProductAgg.Exceptions;
+using Store.DomainModels.ProductOptionAgg.Dtoes;
+using Store.DomainModels.ProductOptionAgg.Requests;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Store.ApplicationServices.ProductOptionAgg.Request
+{
+    public class GetAllProductOptionsHandlerAsync : RequestHandlerAsync<GetAllProductOptions, List<ProductOptionDetails>>
+    {
+        public GetAllProductOptionsHandlerAsync(IUnitOfWork unitOfWork, IEventBus eventBus)
+            : base(unitOfWork, eventBus) { }
+
+        public override async Task<List<ProductOptionDetails>> HandleAsync(GetAllProductOptions req)
+        {
+            var product = await UnitOfWork.Product.GetIncludeOptionsAsync(req.ProductId);
+            if (product is null)
+                throw new ProductNotFoundException();
+
+            return product.Options.Select(s => new ProductOptionDetails(s)).ToList();
+        }
+    }
+}
