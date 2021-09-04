dac9b072b7f178075ae98f149c10b2b529ae3926
M Yaqubi Rad <yqb.rad@gmail.com>
2021-09-04 20:23:13 +0430

    ProductRepository interface updated.

--- 01 Core/02 Contracts/IProductRepository.cs
+++ 01 Core/02 Contracts/IProductRepository.cs
@@ -8,13 +8,13 @@ namespace Store.Contracts
     public interface IProductRepository
     {
         Task CreateAsync(Product product);
-        Task UpdateAsync(Product product);
-        Task DeleteAsync(Product product);
+        void Update(Product product);
+        void Delete(Product product);
 
         Task<Product> GetAsync(Guid id);
         Task<Product> GetIncludeOptionsAsync(Guid id);
         Task<bool> IsExist(string name);
-        Task<IEnumerable<Product>> GetAllAsync();
-        Task<IEnumerable<Product>> SearchAsync(string name);
+        Task<List<Product>> GetAllAsync();
+        Task<List<Product>> SearchAsync(string name);
     }
 }
