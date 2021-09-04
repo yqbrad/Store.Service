914372fcc78e5c1d932a7143c152f9d7bcca2fac
M Yaqubi Rad <yqb.rad@gmail.com>
2021-09-04 20:29:54 +0430

    Product entity type builder completed.

--- 02 Infrastractures/Infrastructures.DataAccess/ProductAgg/ProductConfig.cs
+++ 02 Infrastractures/Infrastructures.DataAccess/ProductAgg/ProductConfig.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Store.DomainModels.ProductAgg.Entities;
+using Store.DomainModels.ProductAgg.ValueObjects;
 
 namespace Store.Infrastructure.DataAccess.ProductAgg
 {
@@ -8,7 +9,38 @@ namespace Store.Infrastructure.DataAccess.ProductAgg
     {
         public void Configure(EntityTypeBuilder<Product> builder)
         {
-            
+            builder.HasKey(s => s.Id);
+
+            builder.Property(s => s.Id)
+                .ValueGeneratedNever();
+
+            builder.Property(s => s.Name)
+                .HasConversion(s => s.Value, s => new ProductName(s))
+                .HasMaxLength(200)
+                .IsRequired()
+                .IsUnicode();
+
+            builder.Property(s => s.Description)
+                .HasConversion(s => s.Value, s => new ProductDescription(s))
+                .IsRequired()
+                .IsUnicode();
+
+            builder.Property(s => s.Price)
+                .HasConversion(s => s.Value, s => new ProductPrice(s))
+                .IsRequired();
+
+            builder.Property(s => s.DeliveryPrice)
+                .HasConversion(s => s.Value, s => new ProductDeliveryPrice(s))
+                .IsRequired();
+
+            builder.HasMany(s => s.Options)
+                .WithOne(s => s.Product)
+                .HasForeignKey(s => s.ProductId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasIndex(s => s.Name)
+                .IsUnique();
         }
     }
 }
